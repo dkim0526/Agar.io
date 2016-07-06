03472c8fed4cf0d9356bcda9cf1469f04369a5cd
Daniel Kim <bjkim0526@gmail.com>
2016-07-06 09:37:26 -0700

    added agar io files

---
+++ Assets/Scripts/Color.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Color : MonoBehaviour {
+
+	public List<Material> Mats = new List<Material>();
+
+	void Awake(){
+		GetComponent<Renderer> ().material = Mats [Random.Range (0, Mats.Count)];	
+
+	}
+}
---
+++ Assets/Scripts/Eat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Eat : MonoBehaviour {
+
+	public string Tag;
+	public float Increase;
+	public Text Letters;
+
+	int Score;
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == Tag) {
+			transform.localScale += new Vector3 (Increase, Increase, Increase);
+			Destroy (other.gameObject);
+
+			Score += 10;
+			Letters.text = "SCORE: " + Score;
+		}
+		
+	}
+}
